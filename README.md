b37ad0723fcdb7e025811e816fb805e5b1c4921e
mozgbrasil <mozgbrasil@gmail.com>
2017-06-27 11:15:39 -0300

    commit - 27/06/2017 11:13:17

---
+++ README.md
@@ -0,0 +1,123 @@
+[checkmark]: https://raw.githubusercontent.com/mozgbrasil/mozgbrasil.github.io/master/assets/images/logos/logo_32_32.png "MOZG"
+![valid XHTML][checkmark]
+
+[psr4]: http://www.php-fig.org/psr/psr-4/
+[getcomposer]: https://getcomposer.org/
+[uninstall-mods]: https://getcomposer.org/doc/03-cli.md#remove
+
+# Hibrido\Trustvox
+
+## Sinopse
+
+Trustvox para Magento
+
+## Motivação
+
+Automação para a instalação do módulo
+
+Para mais informações, visite 
+
+http://help.trustvox.com.br/article/64-instalacao-trustvox-em-uma-loja-com-a-plataforma-magento
+
+Note que não somos o desenvolvedor desta extensão
+
+Esse repositório foi criado com o intuito de instalar o módulo via composer
+
+Neste repositório foi adicionado somente o suporte ao composer e modman.
+
+## Suporte / Dúvidas
+
+Para obter o devido suporte entre em contato com a desenvolvedora do módulo
+
+## Instalação - Atualização - Desinstalação - Desativação
+
+Esta biblioteca destina-se a ser instalado usando o [Composer][getcomposer].
+
+Autoloading compatível é [PSR-4][psr4]
+
+--
+
+Certique se da presença do arquivo composer.json na raiz do seu projeto Magento e que o mesmo tenha os parâmetros semelhantes ao modelo JSON abaixo
+
+	{
+	  "minimum-stability": "dev",
+	  "prefer-stable": true,
+	  "license": [
+	    "proprietary"
+	  ],
+	  "repositories": [
+	    {
+	      "type": "composer",
+	      "url": "https?://packages.firegento.com"
+	    }
+	  ],
+	  "extra": {
+	    "magento-root-dir": "./",
+	    "magento-deploystrategy": "copy",
+	    "magento-force": true
+	  }
+	}
+
+Caso não exista o arquivo composer.json na raiz do projeto Magento, crie o mesmo adicionado o conteúdo acima
+
+--
+
+Para qualquer atualização no Magento sempre mantenha o Compiler e o Cache desativado
+
+--
+
+### Para instalar o módulo execute o comando a seguir no terminal do seu servidor
+
+	composer require mozgbrasil/magento-trustvox:dev-master
+
+Você pode verificar se o módulo está instalado, indo ao backend em:
+
+	STORES -> Configuration -> ADVANCED/Advanced -> Disable Modules Output
+
+--
+
+### Para atualizar o módulo execute o comando a seguir no terminal do seu servidor
+
+	composer clear-cache && composer update
+
+Na ocorrência de erro, renomeie a pasta /vendor/mozgbrasil e execute novamente
+
+Para checar a data do módulo execute o seguinte comando
+
+	grep -ri --include=*.json 'time": "' ./vendor/mozgbrasil
+
+--
+
+### Para [desinstalar][uninstall-mods] o módulo execute o comando a seguir no terminal do seu servidor
+
+	composer remove mozgbrasil/magento-trustvox && composer clear-cache && composer update
+
+--
+
+### Para desativar o módulo
+
+1. Antes de efetuar qualquer processo que envolva atualização sobre o Magento é necessário manter o Compiler e Cache desativado
+
+2. Caso queira desativar os módulos da MOZG renomeie a seguinte pasta app/code/community/Hibrido
+
+A desativação do módulo pode ser usado para detectar se determinada ocorrência tem relação com o módulo
+
+--
+
+## Perguntas mais frequentes "FAQ"
+
+### ?
+
+## Badges
+
+[![Join the chat at https://gitter.im/mozgbrasil](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/mozgbrasil/)
+[![Latest Stable Version](https://poser.pugx.org/mozgbrasil/magento-trustvox/v/stable)](https://packagist.org/packages/mozgbrasil/magento-trustvox)
+[![Total Downloads](https://poser.pugx.org/mozgbrasil/magento-trustvox/downloads)](https://packagist.org/packages/mozgbrasil/magento-trustvox)
+[![Latest Unstable Version](https://poser.pugx.org/mozgbrasil/magento-trustvox/v/unstable)](https://packagist.org/packages/mozgbrasil/magento-trustvox)
+[![License](https://poser.pugx.org/mozgbrasil/magento-trustvox/license)](https://packagist.org/packages/mozgbrasil/magento-trustvox)
+[![Monthly Downloads](https://poser.pugx.org/mozgbrasil/magento-trustvox/d/monthly)](https://packagist.org/packages/mozgbrasil/magento-trustvox)
+[![Daily Downloads](https://poser.pugx.org/mozgbrasil/magento-trustvox/d/daily)](https://packagist.org/packages/mozgbrasil/magento-trustvox)
+[![Reference Status](https://www.versioneye.com/php/mozgbrasil:magento-trustvox/reference_badge.svg?style=flat-square)](https://www.versioneye.com/php/mozgbrasil:magento-trustvox/references)
+[![Dependency Status](https://www.versioneye.com/php/mozgbrasil:magento-trustvox/1.0.0/badge?style=flat-square)](https://www.versioneye.com/php/mozgbrasil:magento-trustvox/1.0.0)
+
+:cat2:
---
+++ app/code/community/Hibrido/Trustvox/controllers/OrdersController.php
@@ -0,0 +1,120 @@
+<?php
+
+class Hibrido_Trustvox_OrdersController extends Mage_Core_Controller_Front_Action {
+
+    private function helper()
+    {
+        return Mage::helper('hibridotrustvox');
+    }
+
+    public function indexAction()
+    {
+        $this->getResponse()->clearHeaders()->setHeader('Content-type', 'application/json', true);
+
+        $store_id = Mage::app()->getStore()->getId();
+        if($this->getRequest()->getHeader('store_id') && $this->getRequest()->getHeader('store_id') != ''){
+            $store_id = $this->getRequest()->getHeader('store_id');
+        }
+
+        if ($this->getRequest()->getHeader('trustvox-token') == $this->helper()->getToken($store_id)) {
+            $counter = 0;
+            $sent = 0;
+            $clientArray = array();
+            $productArray = array();
+
+            if($this->getRequest()->getHeader('date-period') && $this->getRequest()->getHeader('date-period') >= 1){
+                $period = intval($this->getRequest()->getHeader('date-period'));
+            }else{
+                $period = 15;
+            }
+
+            $lastSync = $this->getRequest()->getHeader('last-sync');
+            if($lastSync != '' && $lastSync >= 1){
+                $period = strtotime(date('Y-m-d H:i:s')) - strtotime($lastSync);
+                $period = floor($period / (60 * 60 * 24));
+            }
+
+            $orders = $this->helper()->getOrdersByLastDays($period, $store_id);
+
+            $json = array();
+
+            $i = -1;
+            foreach ($orders as $order) {
+                ++$i;
+                $clientArray = $this->helper()->mountClientInfoToSend($order->getCustomerFirstname(), $order->getCustomerLastname(), $order->getCustomerEmail());
+
+                $enabled = $this->helper()->checkStoreIdEnabled();
+                $productArray = array();
+
+
+                foreach ($order->getAllItems() as $item) {
+                    $_product = Mage::getModel('catalog/product');
+
+                    if ($item->getProductType() == 'simple') {
+                        $parents = Mage::getResourceSingleton('catalog/product_type_configurable')->getParentIdsByChild($item->getProductId());
+                        if(count($parents) >= 1){
+                            $productId = $parents[0];
+                        }else{
+                            $productId = $item->getProductId();
+                        }
+                    }else if($item->getProductType() == 'grouped'){
+                        $parents = Mage::getModel('catalog/product_type_grouped')->getParentIdsByChild($item->getProductId());
+                        if(count($parents) >= 1){
+                            $productId = $parents[0];
+                        }else{
+                            $productId = $item->getProductId();
+                        }
+                    }else{
+                        $productId = $item->getProductId();
+                    }
+
+                    if ($item->getParentItemId()) {
+                        $parent_product_type = Mage::getModel('sales/order_item')->load($item->getParentItemId())->getProductType();
+                        if ($parent_product_type == Mage_Catalog_Model_Product_Type::TYPE_BUNDLE) {
+                            $productId = $item->getParentItemId();
+                        }
+                    }
+
+                    $_item = $_product->load($productId);
+                    $product_url = $_item->getProductUrl();
+
+                    $images = array();
+
+                    foreach ($_item->getMediaGalleryImages() as $image) {
+                        array_push($images, $image->getUrl());
+                    }
+
+                    if($_item->getId()){
+                        $productArray[$_item->getId()] = array(
+                            'name' => $_item->getName(),
+                            'id' => $_item->getId(),
+                            'price' => $_item->getPrice(),
+                            'url' => $product_url,
+                            'type' => $item->getProductType(),
+                            'photos_urls' => is_null($images[0]) ? '' : array($images[0]),
+                        );
+                    }
+                }
+
+            $shippingDate = '';
+            foreach($order->getShipmentsCollection() as $shipment){
+                $shippingDate = $shipment->getCreatedAt();
+            }
+
+            if(!$shippingDate || $shippingDate == ''){
+                $shippingDate = $order->getCreatedAt();
+            }
+
+            array_push($json, $this->helper()->forJSON($order->getId(), $shippingDate, $clientArray, $productArray));
+            }
+
+            return $this->getResponse()->setBody(json_encode($json));
+        } else {
+            $jsonArray = array(
+                'error' => true,
+                'message' => 'not authorized',
+            );
+            $this->getResponse()->setBody(json_encode($jsonArray));
+        }
+    }
+}
